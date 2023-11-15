4f501dca0ff769f4d2a8b09f231174e1e560399f
LYNX - Duc Minh Vu <d.m.vu@de-nb-029.local>
2023-11-15 16:06:34 +0100

    added revenue via res.currency

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -4,16 +4,21 @@ from odoo import models, fields, api
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    planned_revenue_viet = fields.Float('Planned Revenue VND',
-                                       default="Ask me tomorrow",
-                                       compute="_compute_planned_revenue_viet",
-                                       track_visibility='always')
+    planned_revenue_viet = fields.Monetary(string='Planned Revenue VND',
+                                           currency_field='currency_id',
+                                           compute="_compute_planned_revenue_viet",
+                                           #                                     track_visibility='always'
+                                           )
+    currency_id = fields.Many2one('res.currency', string='Currency')
 
-    @api.depends("planned_revenue")
+
+
+    @api.depends("planned_revenue", "currency_id")
     def _compute_planned_revenue_viet(self):
         """
         compute the VND from planned revenue
         """
-        for rec in self:
-            rec.planned_revenue_viet = rec.planned_revenue * 26088
 
+        for rec in self:
+            rec.planned_revenue_viet = rec.planned_revenue * rec.currency_id.rate
+            print(f'rec.planned_revenue: {rec.planned_revenue} rec.currency_id.rate: {rec.currency_id.rate}')
